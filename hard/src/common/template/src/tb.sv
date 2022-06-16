e2259fe3ba1fd9113e3ea988bf8a3ce895aa0597
Василий <seovasso@gmail.com>
2022-06-16 19:31:43 +0300

    [hard][sim]Дополнен пример юнит теста, чтобы он выводил результат проверки в консоль

--- hard/src/common/template/src/tb.sv
+++ hard/src/common/template/src/tb.sv
@@ -7,6 +7,8 @@ module tb ();
   // Clock/reset generation
   logic clk = 1'b0;
   logic rstn = 1'b0;
+  logic d_i = 1'b1;
+  logic d_o;
 
   always #(`CLK_PERIOD/2) clk = !clk;
   initial #(`RESET_GOES_HIGH) rstn = 1'b1;
@@ -15,9 +17,26 @@ module tb ();
     .clk_i (clk ),
     .rstn_i(rstn),
 
-    .d_i   (1'b1),
-    .d_o   () 
+    .d_i   (d_i),
+    .d_o   (d_o)
   );
 
+  logic ok = 1;
+  initial begin
+    $display("TEST STARTED");
+    // we wait start
+    #(`RESET_GOES_HIGH);
+    #(`CLK_PERIOD);
 
+
+    if (d_o != 1'b1) ok = 0;
+
+    d_i = 0;
+    #(`CLK_PERIOD);
+    if (d_o != 1'b0) ok = 0;
+
+    if (ok == 1) $display("TEST PASSED");
+    else         $display("TEST FAILED");
+    $stop();
+  end
 endmodule // tb
