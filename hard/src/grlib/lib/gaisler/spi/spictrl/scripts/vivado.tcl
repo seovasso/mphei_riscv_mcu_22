1d76d0989569ea65bac8eedd851a248e815d709d
fhvlegenda <fhv555@yandex.ru>
2022-07-15 11:37:30 +0300

    Smth

--- hard/src/grlib/lib/gaisler/spi/spictrl/scripts/vivado.tcl
+++ hard/src/grlib/lib/gaisler/spi/spictrl/scripts/vivado.tcl
@@ -14,22 +14,30 @@ set_property board_part xilinx.com:kc705:part0:1.6 [current_project]
 
 # add source
 create_fileset $COMMON_FILESET
-add_files -norecurse ../../../../../../grlib/lib/gaisler/spi/spictrl/src/vhdl2verilog.vhd
+add_files -norecurse ../../../../../../grlib/lib/gaisler/spi/spictrl/src/spi_wrap.vhd
 
 
-read_vhdl -vhdl2008 ../../../../../../grlib/lib/gaisler/spi/spi.vhd           -library gaisler
-read_vhdl -vhdl2008 ../../../../../../grlib/lib/grlib/amba/amba.vhd       -library grlib
-read_vhdl -vhdl2008 ../../../../../../grlib/lib/grlib/stdlib/config.vhd
-read_vhdl -vhdl2008 ../../../../../../grlib/lib/grlib/stdlib/config_types.vhd         -library gaisler
-read_vhdl -vhdl2008 ../../../../../../grlib/lib/grlib/amba/devices.vhd       -library grlib
-read_vhdl -vhdl2008 ../../../../../../grlib/lib/gaisler/spi/spictrl.vhd            -library gaisler
-read_vhdl -vhdl2008 ../../../../../../grlib/lib/gaisler/spi/spictrlx.vhd           -library gaisler
+read_vhdl -vhdl2008 ../../../../../../grlib/lib/gaisler/spi/spi.vhd        		-library gaisler
+read_vhdl -vhdl2008 ../../../../../../grlib/lib/grlib/amba/amba.vhd      		-library grlib
+read_vhdl -vhdl2008 ../../../../../../grlib/lib/grlib/stdlib/config.vhd			-library grlib
+read_vhdl -vhdl2008 ../../../../../../grlib/lib/grlib/stdlib/config_types.vhd   -library grlib
+read_vhdl -vhdl2008 ../../../../../../grlib/lib/grlib/amba/devices.vhd       	-library grlib
+read_vhdl -vhdl2008 ../../../../../../grlib/lib/gaisler/spi/spictrl.vhd         -library gaisler
+read_vhdl -vhdl2008 ../../../../../../grlib/lib/gaisler/spi/spictrlx.vhd        -library gaisler
 read_vhdl -vhdl2008 ../../../../../../grlib/lib/grlib/stdlib/stdlib.vhd         -library grlib
-read_vhdl -vhdl2008 ../../../../../../grlib/lib/grlib/stdlib/version.vhd                 -library gaisler
+read_vhdl -vhdl2008 ../../../../../../grlib/lib/grlib/stdlib/version.vhd        -library grlib
 
 read_vhdl -vhdl2008 ../../../../../../grlib/lib/techmap/gencomp/gencomp.vhd       -library techmap
 read_vhdl -vhdl2008 ../../../../../../grlib/lib/techmap/gencomp/netcomp.vhd       -library techmap
 
 
+# add simulation source
+create_fileset $SIM_FILESET
+add_files -norecurse ../../../../../../grlib/lib/gaisler/spi/spictrl/src/tb.sv
+
+set_property top tb [get_filesets sim_1]
+
+# launch simulation
+launch_simulation
 
 start_gui
--- hard/src/grlib/lib/gaisler/spi/spictrl/src/tb.sv
+++ hard/src/grlib/lib/gaisler/spi/spictrl/src/tb.sv
@@ -7,36 +7,68 @@ module tb ();
   // Clock/reset generation
   logic clk = 1'b0;
   logic rstn = 1'b0;
-  logic d_i = 1'b1;
-  logic d_o;
+  logic spii_cstart = 1'b0;
+  logic spii_astart = 1'b0;
+//  logic d_i = 1'b1;
+//  logic d_o;
+
+  logic psel = 1'b0;
+  logic penable = 1'b0;
+  reg paddr = 32'b10001;
+  reg pwdata = 32'b1000000001;
+  logic pwrite = 1'b0;
 
   always #(`CLK_PERIOD/2) clk = !clk;
   initial #(`RESET_GOES_HIGH) rstn = 1'b1;
 
-  top uut(
-    .clk_i (clk ),
-    .rstn_i(rstn),
-
-    .d_i   (d_i),
-    .d_o   (d_o)
+  spi_wrap uut(
+    .clk (clk ) ,
+    .rstn(rstn) ,
+    .apbi_psel   (psel),
+    .apbi_penable(penable),
+    .apbi_paddr  (paddr),
+    .apbi_pwrite (pwrite),
+    .apbi_pwdata (pwdata),
+    .apbi_testen (),
+    .apbi_testrst(),
+    .apbi_scanen (),
+    .apbi_testoen(),
+    .apbo_prdata (),
+    .apbo_pirq   (),
+    .spii_miso   (spii_miso),
+    .spii_mosi   (spii_mosi),
+    .spii_sck    (sck),
+    .spii_spisel (),
+    .spii_astart (),
+    .spii_cstart (),
+    .spii_ignore (),
+    .spii_io2    (),
+    .spii_io3    (),
+    .spio_miso   (spio_miso),
+    .spio_misooen(),
+    .spio_mosi   (spio_mosi),
+    .spio_mosioen(),
+    .spio_sck    (),
+    .spio_sckoen (),
+    .spio_enable (),
+    .spio_astart (),
+    .spio_aready (),
+    .spio_io2    (),
+    .spio_io2oen (),
+    .spio_io3    (),
+    .spio_io3oen (),
+    .slvsel      (slvsel)
   );
 
-  logic ok = 1;
+//  logic ok = 1;
   initial begin
     $display("TEST STARTED");
     // we wait start
     #(`RESET_GOES_HIGH);
     #(`CLK_PERIOD);
 
-
-    if (d_o != 1'b1) ok = 0;
-
-    d_i = 0;
-    #(`CLK_PERIOD);
-    if (d_o != 1'b0) ok = 0;
-
-    if (ok == 1) $display("TEST PASSED");
-    else         $display("TEST FAILED");
-    $stop();
+    #40 pwrite <= 1;
+    #20 penable <= 1;
+    #30 psel <= 1;
   end
 endmodule // tb
