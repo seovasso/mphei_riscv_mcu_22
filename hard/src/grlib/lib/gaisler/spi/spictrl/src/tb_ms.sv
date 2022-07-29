6e1e8f20eec7ae96e59e2c1c822485660ef7c309
fhvlegenda <fhv555@yandex.ru>
2022-07-29 17:05:01 +0300

    Обновление
    Не выходит правильно подключить slvsel к соответствующим spii_spisel. Сами slvsel выдают на выходе "1", но соответствующие им переменные типа wire равны "X", и, соответственно, на spii_spisel передаются "U"

--- hard/src/grlib/lib/gaisler/spi/spictrl/src/tb_ms.sv
+++ hard/src/grlib/lib/gaisler/spi/spictrl/src/tb_ms.sv
@@ -10,23 +10,19 @@ module tb  ();
   logic [31:0]    apb_prdata;
   logic [31:0]    apb_pwdata;
   logic [31:0]    apb_paddr;
+  logic           spisel = 1'b1;
   logic           zero = 1'b0;
   logic           one = 1'b1;
-  
   
   logic [31:0]    apb_prdata_slave;
   logic [31:0]    apb_pwdata_slave;
   logic [31:0]    apb_paddr_slave;
-
-    
+  
+  
   always #(`CLK_PERIOD/2) clk = !clk;
   initial #(`RESET_GOES_HIGH) rstn = 1'b1;
 
-  // APB Interface
   apb_if    apb( .pclk(clk) );
-  apb_if    apb_slave( .pclk(clk) );
-  
-  // Master
   assign apb.mst_tb.prdata  = apb_prdata;
   assign apb.mst_tb.pready  = 1'b1;
   assign apb.mst_tb.pslverr = 1'b0;
@@ -36,27 +32,18 @@ module tb  ();
   assign apb_pwdata         = apb.mst_tb.pwdata;
   assign apb_paddr          = apb.mst_tb.paddr;
   
-  assign apb_slave.mst_tb.prdata  = apb_prdata_slave;
-  assign apb_slave.mst_tb.pready  = 1'b1;
-  assign apb_slave.mst_tb.pslverr = 1'b0;
-  assign apb_psel_slave           = apb_slave.mst_tb.psel;
-  assign apb_penable_slave        = apb_slave.mst_tb.penable;
-  assign apb_pwrite_slave         = apb_slave.mst_tb.pwrite;
-  assign apb_pwdata_slave         = apb_slave.mst_tb.pwdata;
-  assign apb_paddr_slave          = apb_slave.mst_tb.paddr;
-  
-  wire miso;
-  wire mosi;
+  assign apb.slv_tb.paddr   = apb_prdata_slave;
+  assign apb.slv_tb.penable = apb_penable_slave;
+  assign apb.slv_tb.pwrite  = apb_pwrite_slave;
+  assign apb.slv_tb.pwdata  = apb_pwdata_slave;
+  assign apb.slv_tb.psel    = apb_psel_slave;
+  assign apb.slv_tb.pstrb   = 1'b0;
+  assign apb.slv_tb.pprot   = 1'b0;
+  assign apb_pready_slave   = apb.slv_tb.pready;
+  assign apb_prdata_slave   = apb.slv_tb.prdata;
+  assign apb_pslverr_slave  = apb.slv_tb.pslverr;
  
-  wire mosi2;
-  wire miso2;
-  wire clkgen;
-  wire clkgen2;
-  
-  wire selectms;
-  wire selectsm;
-  
-  spi_wrap #(.syncram(0), .twen(0), .prot(0), .slvselen(1)) master(
+  spi_wrap #(.syncram(0)) master(
   
     .clk (clk ) ,
     .rstn(rstn) ,
@@ -71,20 +58,20 @@ module tb  ();
     .apbi_testoen(),
     .apbo_prdata (apb_prdata),
     .apbo_pirq   (),
-    .spii_miso   (miso),
-    .spii_mosi   (mosi2),
-    .spii_sck    (clkgen2),
-    .spii_spisel (selectsm),
+    .spii_miso   (),
+    .spii_mosi   (zero),
+    .spii_sck    (clk),
+    .spii_spisel (spisel),
     .spii_astart (zero),
     .spii_cstart (zero),
     .spii_ignore (one),
     .spii_io2    (zero),
     .spii_io3    (zero),
-    .spio_miso   (miso2),
+    .spio_miso   (spio_miso),
     .spio_misooen(),
-    .spio_mosi   (mosi),
+    .spio_mosi   (spio_mosi),
     .spio_mosioen(),
-    .spio_sck    (clkgen),
+    .spio_sck    (),
     .spio_sckoen (),
     .spio_enable (),
     .spio_astart (),
@@ -93,10 +80,10 @@ module tb  ();
     .spio_io2oen (),
     .spio_io3    (),
     .spio_io3oen (),
-    .slvsel      (selectms)
+    .slvsel      ()
   );
   
-  spi_wrap #(.syncram(0), .twen(0), .prot(0), .slvselen(1)) slave(
+  spi_wrap #(.syncram(0), .twen(0), .prot(1)) slave(
   
     .clk (clk ) ,
     .rstn(rstn) ,
@@ -111,20 +98,20 @@ module tb  ();
     .apbi_testoen(),
     .apbo_prdata (apb_prdata_slave),
     .apbo_pirq   (),
-    .spii_miso   (miso2),
-    .spii_mosi   (mosi),
-    .spii_sck    (clkgen),
-    .spii_spisel (selectms),
+    .spii_miso   (spio_miso),
+    .spii_mosi   (spio_mosi),
+    .spii_sck    (clk),
+    .spii_spisel (spisel),
     .spii_astart (zero),
     .spii_cstart (zero),
     .spii_ignore (one),
     .spii_io2    (zero),
     .spii_io3    (zero),
-    .spio_miso   (miso),
+    .spio_miso   (),
     .spio_misooen(),
-    .spio_mosi   (mosi2),
+    .spio_mosi   (),
     .spio_mosioen(),
-    .spio_sck    (clkgen2),
+    .spio_sck    (),
     .spio_sckoen (),
     .spio_enable (),
     .spio_astart (),
@@ -133,42 +120,40 @@ module tb  ();
     .spio_io2oen (),
     .spio_io3    (),
     .spio_io3oen (),
-    .slvsel      (selectsm)
+    .slvsel      ()
   );
   
+  
+  
+  
   initial begin
+  
     localparam CM_EN = 24;
     localparam CM_MS = 25;
     localparam CM_ASEL = 14;
-    localparam CC_LST = 22;
-    localparam CE_LT = 14;
-    localparam CM_REV = 26;
-    localparam PM = 16;
+    localparam CM_TW = 15;
     apb.mst_tb.init;
-    apb_slave.mst_tb.init;
-    
+
     #(`RESET_GOES_HIGH);
     #(`CLK_PERIOD);
     
-    apb_slave.mst_tb.write(
-    (32'h0000_0000 | 1 << CM_EN | 0 << CM_MS | 1 << PM), 32'h20); 
-    apb.mst_tb.write( 
-    (32'h0000_0000 | 1 << CM_EN | 1 << CM_MS | 1 << PM), 32'h20); 
-    apb.mst_tb.write( 
-    (32'hFFFF_FFFF), 32'h38); 
-    apb_slave.mst_tb.write( 
-    (32'hFFFF_FFFF), 32'h38);     
-    
     apb.mst_tb.cyc_wait(20);
-    apb.mst_tb.write( 32'h12132431, 32'h30); 
-    apb_slave.mst_tb.write( 32'h1700, 32'h30);
-    apb.mst_tb.read( apb_prdata, 32'h34);
-    //apb.mst_tb.cyc_wait(60);
-    //apb_slave.mst_tb.read( apb_prdata_slave, 32'h34);
-    //apb.mst_tb.write( 32'h2343_2332, 32'h30); 
-    //apb.mst_tb.cyc_wait(200);
-    //apb_slave.mst_tb.write( 32'h212_21, 32'h30); 
-
+    apb.slv_tb.write( 
+    (32'h0000_0000 | 1 << CM_EN | 1 << CM_ASEL | 1 << CM_TW)
+    ,32'h20); 
+    apb.mst_tb.write( 
+    (32'h0000_0000 | 1 << CM_EN | 1 << CM_MS | 1 << CM_ASEL | 1 << CM_TW)
+    ,32'h20); 
+    
+    // Master's problems
+    apb.mst_tb.cyc_wait(20);
+    apb.mst_tb.write( 32'h2421980, 32'h30);     
+    
+    // Slave's problems
+    
+    apb.mst_tb.read(apb_prdata_slave, 32'h34);
+    
+   // apb.mst_tb.read( apb_prdata_master, 32'h00); 
   end
   
 endmodule // tb
